4e8d3b19bf01bf97fe5a52ff4c87806178f93f3f
Andrew Howes <andrew@viamedia.co.za>
2020-03-29 14:14:37 +0200

    v1.0.0: Initial commit of the Kafka library

---
+++ src/main/java/com/kafka/config/KafkaConfig.java
@@ -0,0 +1,142 @@
+package com.kafka.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.kafka.listener.config.ContainerProperties;
+import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+@Configuration
+public class KafkaConfig<T> {
+
+	@Value("${kafka.server:localhost:9092}")
+	private String bootstrapServers;
+
+	@Value("${kafka.default.synchronous.group.id:DEFAULT_SYNCHRONOUS_ID")
+	private String defaultSynchronousGroupId;
+	
+	@Value("${kafka.default.synchronous.topic:DEFAULT_SYNCHRONOUS_TOPIC}")
+	private String defaultSynchronousTopic;
+
+//	PRODUCER CONFIG
+//	###############
+	@Bean
+	public Map<String, Object> producerConfigs() {
+		Map<String, Object> props = new HashMap<>();
+
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+		return props;
+	}
+	
+	@Bean
+	public ProducerFactory<String, T> producerFactory() {
+		return new DefaultKafkaProducerFactory<>(producerConfigs());
+	}
+
+	@Bean
+	public KafkaTemplate<String, T> kafkaTemplate() {
+		return new KafkaTemplate<>(producerFactory());
+	}
+//	###############
+	
+	
+	
+	
+//	CONSUMER CONFIG
+//	###############
+	@Bean
+	public Map<String, Object> consumerConfigs() {
+		Map<String, Object> props = new HashMap<>();
+
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, defaultSynchronousGroupId);
+
+		return props;
+	}
+	
+	@Bean
+	public ConsumerFactory<String, T> consumerFactory() {
+		final JsonDeserializer<T> jsonDeserializer = new JsonDeserializer<>();
+
+		jsonDeserializer.addTrustedPackages("*");
+
+		return new DefaultKafkaConsumerFactory<>(consumerConfigs(), new StringDeserializer(), jsonDeserializer);
+	}
+	
+	@Bean
+	public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, T>> kafkaListenerContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<String, T> factory = new ConcurrentKafkaListenerContainerFactory<>();
+
+		factory.setConsumerFactory(consumerFactory());
+		factory.setReplyTemplate(kafkaTemplate());
+
+		return factory;
+	}
+	
+    @Bean
+    public ConsumerFactory<String, byte[]> byteArrayConsumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs(), new StringDeserializer(), new ByteArrayDeserializer());
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, byte[]> kafkaListenerByteArrayContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, byte[]> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        
+        factory.setConsumerFactory(byteArrayConsumerFactory());
+        
+        return factory;
+    }
+    
+    @Bean
+    public ConsumerFactory<String, String> stringConsumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs(), new StringDeserializer(), new StringDeserializer());
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerStringContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        
+        factory.setConsumerFactory(stringConsumerFactory());
+
+        return factory;
+    }    
+//	###############
+    
+    
+
+//	SYNCHRONOUS CONFIG
+//	##################
+	@Bean
+	public ReplyingKafkaTemplate<String, T, T> replyKafkaTemplate(ProducerFactory<String, T> pf,KafkaMessageListenerContainer<String, T> container) {
+		return new ReplyingKafkaTemplate<>(pf, container);
+	}
+
+	@Bean
+	public KafkaMessageListenerContainer<String, T> replyContainer(ConsumerFactory<String, T> cf) {
+		ContainerProperties containerProperties = new ContainerProperties(defaultSynchronousTopic);
+		return new KafkaMessageListenerContainer<>(cf, containerProperties);
+	}
+//	##################
+}
